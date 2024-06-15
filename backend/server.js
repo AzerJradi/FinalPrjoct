e3fbb0b586f05c4c60e03ffd22c27d96c3fd2e99
AzerJradi <azerjradi13@gmail.com>
2024-06-15 20:06:41 +0100

    last commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,48 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.js"
-import connectDB from "./db/connectDB.js"
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT
-app.use("/api/auth",authRoutes)
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
-app.listen(PORT,()=>{
-    console.log(`server is runnig on port: ${PORT}`)
-    connectDB()
-})
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
